0996ebab68f7d6f9c08858bf14fca36e6133457c
bwjones91 <bwjones1991@gmail.com>
2018-01-31 21:56:23 -0500

    Spotlight Lerp

--- Mining Room/Assets/Scripts/CameraChildDetachment.cs
+++ Mining Room/Assets/Scripts/CameraChildDetachment.cs
@@ -10,14 +10,24 @@ public class CameraChildDetachment : MonoBehaviour {
     public GameObject funnelFloor;
     public GameObject spotLight;
 
-    void Start() {
+    public Transform startMarker;
+    public Transform endMarker;
+    public float speed = 1.0f;
+    private float startTime;
+    private float journeyLength;
 
+    private bool isLerp = false;
+
+    void Start() {
+        
+        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
     }
 
     void Update() {
-
-
-
+        if (isLerp)
+        {
+            SpotlightPositionChanging();
+        }
 	}
 
     private void OnTriggerEnter(Collider other)
@@ -25,7 +35,27 @@ public class CameraChildDetachment : MonoBehaviour {
         myCameraObject.parent = null;
         funnel.transform.position = new Vector3(225.5f, 234f, 35.3f);
         funnelFloor.SetActive(false);
-        spotLight.SetActive(true);
+
+        startTime = Time.time;
+        isLerp = true;
+        
+
+        //StartCoroutine(MoveSpotlight());
+        //yield return new WaitForSeconds(2f);
+        //spotLight.transform.position = new Vector3(234.4f, 234f, 72.5f);
     }
+
+    void SpotlightPositionChanging()
+    {
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        spotLight.transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+    }
+
+    /*IEnumerator MoveSpotlight()
+    {
+        yield return new WaitForSeconds(2f);
+        spotLight.transform.position = new Vector3(234.4f, 234f, 72.5f);
+    }*/
 
 }
