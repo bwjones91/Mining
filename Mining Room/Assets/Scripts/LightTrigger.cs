cafda021d65c70d9a2b0bcfac9bc763d78e805e9
bwjones91 <bwjones1991@gmail.com>
2018-02-02 16:24:35 -0500

    Worked on lighting

---
+++ Mining Room/Assets/Scripts/LightTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTrigger : MonoBehaviour {
+
+    public GameObject torch;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        torch.gameObject.SetActive(true);
+    }
+
+}
